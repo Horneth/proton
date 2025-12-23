2161bd880ae52cc6cc53d0a0facc620b18bec8c4
Thibault Jeandet <thibault@insitia.co>
2025-12-23 15:12:25 +0100

    daml decode

--- cmd/proton/cmd_daml.go
+++ cmd/proton/cmd_daml.go
@@ -9,6 +9,7 @@ import (
 	"buf-lib-poc/pkg/io"
 
 	"github.com/spf13/cobra"
+	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -50,6 +51,38 @@ func initDamlCommands(rootCmd *cobra.Command) {
 	}
 	// hashCmd.Flags().BoolVarP(&base64HashFlag, "base64", "b", false, "Output hash as base64")
 
+	var decodeCmd = &cobra.Command{
+		Use:   "decode [file]",
+		Short: "Decode a binary PreparedTransaction into JSON",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			inputPath := args[0]
+			data, err := io.ReadData(inputPath, false)
+			if err != nil {
+				log.Fatalf("failed to read input file: %v", err)
+			}
+
+			var preparedTx interactive.PreparedTransaction
+			if err := proto.Unmarshal(data, &preparedTx); err != nil {
+				log.Fatalf("failed to unmarshal prepared transaction: %v", err)
+			}
+
+			// Marshal to JSON using protojson for proper enum/field name handling
+			m := protojson.MarshalOptions{
+				Multiline:       true,
+				Indent:          "  ",
+				EmitUnpopulated: false,
+			}
+			jsonBytes, err := m.Marshal(&preparedTx)
+			if err != nil {
+				log.Fatalf("failed to marshal to JSON: %v", err)
+			}
+
+			fmt.Println(string(jsonBytes))
+		},
+	}
+
 	damlCmd.AddCommand(hashCmd)
+	damlCmd.AddCommand(decodeCmd)
 	rootCmd.AddCommand(damlCmd)
 }
